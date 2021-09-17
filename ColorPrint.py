a6e8b7d789c8e70a7687e8173c69e9f2fa8238ae
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2021-09-17 19:35:54 +0200

    Update ColorPrint.py

--- ColorPrint.py
+++ ColorPrint.py
@@ -1,13 +1,32 @@
-def colorprint(text,color="",background="",gras=False,ligne=False):
+'''
+--|~|--|~|--|~|--|~|--|~|--|~|--
+
+██  ████        ██████        ██
+████    ██     ██           ████
+██      ██   ████████     ██  ██
+████████       ██       ██    ██
+██             ██       █████████
+██             ██             ██
+               ██
+.codé en : UTF-8
+.langage : python 3
+.note    :
+--|~|--|~|--|~|--|~|--|~|--|~|--
+'''
+
+def colorprint(text,color="",background="",gras=False,ligne=False,end=True):
     style = ""
     if gras == True and ligne == True:
-        print(f"{color}\033[01m\033[04m{background}"+text+f"\033[0m")
+        print(f"{color}\033[01m\033[04m{background}"+text+f"\033[0m",end="")
     else:
         if gras == False and ligne == True:
             style = "\033[01m"
         elif gras == True and ligne == False:
             style = "\033[04m"
-        print(f"{color}{style}{background}"+text+f"\033[0m")
+        print(f"{color}{style}{background}"+text+f"\033[0m",end="")
+    if end:
+        print("")
+
 
 def colorinput(text,color="",background="",gras=False,ligne=False):
     style = ""
